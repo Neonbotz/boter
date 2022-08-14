b9f42386d1ed4efecb762ea215212d07dc253c4f
Neon mD <106319373+Neonbotz@users.noreply.github.com>
2022-08-14 18:24:33 +0700

    Update owner-setbotpp.js

--- plugins/owner-setbotpp.js
+++ plugins/owner-setbotpp.js
@@ -1,22 +1,32 @@
-/*
-   Silahkan Di Pakek
-   Tapi Bantu Rapihin :v
-   Buatan: Miaweers
-*/
-
-let handler = async (m, { conn, args }) => {
-    let bot = conn.user.jid // Bot
-    let q = m.quoted ? m.quoted : m
-    let mime = (q.msg || q).mimetype || ''
-    if (/image/.test(mime)) {
-      let img = await q.download()
-      if (!img) throw `Foto tidak ditemukan`
-     conn.updateProfilePicture (bot, img)
-    conn.reply(m.chat, 'Sukses Mengganti Foto Profile Bot!', m)
-	}
-    }
+const fs = require('fs')
+let handler = async (m, { conn, isROwner }) => {
+ if (!isROwner) throw false
+let q = m.quoted ? m.quoted : m
+let mime = (q.msg || q).mimetype || ''
+var media = await q.download()
+            let botNumber = await conn.user.jid
+            var { img } = await conn.generateProfilePicture(media)
+            await conn.query({
+            tag: 'iq',
+            attrs: {
+            to: botNumber,
+            type:'set',
+            xmlns: 'w:profile:picture'
+            },
+            content: [
+            {
+            tag: 'picture',
+            attrs: { type: 'image' },
+            content: img
+            }
+            ]
+            })
+            m.reply(`Sukses ✔︎`)
+}
 handler.help = ['setbotpp']
-handler.command = /^(setbotpp)$/i
+handler.tags = ['owner']
+handler.command = /^(set(bot)?pp)$/i
+
 handler.owner = true
 
 module.exports = handler
