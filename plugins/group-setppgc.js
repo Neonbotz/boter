6d5259115547a07ae1eddc17ef0be73ba3ec3a59
Neon mD <106319373+Neonbotz@users.noreply.github.com>
2022-08-14 18:20:42 +0700

    Update group-setppgc.js

--- plugins/group-setppgc.js
+++ plugins/group-setppgc.js
@@ -1,19 +1,33 @@
-let handler = async (m, { conn, usedPrefix, command }) => {
-    let q = m.quoted ? m.quoted : m
-    let mime = (q.msg || q).mimetype || ''
-    if (/image/.test(mime)) {
-        let img = await q.download()
-        if (!img) throw 'Gambar tidak ditemukan'
-        await conn.updateProfilePicture(m.chat, img)
-    } else throw `kirim/balas gambar dengan caption *${usedPrefix + command}*`
+const fs = require('fs')
+let handler = async (m, { conn, isROwner }) => {
+ if (!isROwner) throw false
+let q = m.quoted ? m.quoted : m
+let mime = (q.msg || q).mimetype || ''
+var media = await q.download()
+            let gc = m.chat
+            var { img } = await conn.generateProfilePicture(media)
+            await conn.query({
+            tag: 'iq',
+            attrs: {
+            to: gc,
+            type:'set',
+            xmlns: 'w:profile:picture'
+            },
+            content: [
+            {
+            tag: 'picture',
+            attrs: { type: 'image' },
+            content: img
+            }
+            ]
+            })
+            m.reply(`Sukses ✔︎`)
 }
-handler.help = ['setpp']
+handler.help = ['setppgc']
 handler.tags = ['group']
-
-handler.command = /^setpp$/i
+handler.command = /^(setppgc)$/i
 
 handler.group = true
 handler.admin = true
 handler.botAdmin = true
-
 module.exports = handler
